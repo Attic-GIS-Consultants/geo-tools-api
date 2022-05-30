6e8295e6700adf289f5d6326f0f44d72ea1c5238
GISSIAH <62018768+GISSIAH@users.noreply.github.com>
2022-05-30 15:58:48 +0200

    added error handling on converter route

--- src/coordinates/coordinates.controller.ts
+++ src/coordinates/coordinates.controller.ts
@@ -1,17 +1,58 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Request ,Response} from 'express';
 import { CoordinatesService } from './coordinates.service';
 
 @Controller('coordinates')
 export class CoordinatesController {
   constructor(private readonly coordinateService: CoordinatesService) {}
   @Post('convert')
-  convertCoordinates(@Req() req: Request): { lat: number; lon: number } {
+  convertCoordinates(@Req() req: Request, @Res() res: Response) {
+    /*
     const tempCoords = {
       lat: Number(req.body.lat),
       lon: Number(req.body.lon),
     };
+*/
+    if (checkUnitToConvert(req.body) === 'empty') {
+      res.statusCode = 400;
+      res.send({ error: ' Found no coordinates in body' });
+    } else if (checkUnitToConvert(req.body) === 'meters') {
+      const tempCoords = {
+        lat: Number(req.body.meters[1]),
+        lon: Number(req.body.meters[0]),
+      };
+      res.send(this.coordinateService.convertCoordinates(tempCoords, 'meters'));
+    } else if (checkUnitToConvert(req.body) === 'degrees') {
+      const tempCoords = {
+        lat: Number(req.body.degrees[1]),
+        lon: Number(req.body.degrees[0]),
+      };
+      res.send(
+        this.coordinateService.convertCoordinates(tempCoords, 'degrees'),
+      );
+    } else {
+      res.statusCode = 400;
+      res.send({ error: 'One pair must have the values you want to change' });
+    }
+  }
+}
 
-    return this.coordinateService.convertCoordinates(tempCoords);
+function checkUnitToConvert(body: {
+  degrees: Array<number>;
+  meters: Array<number>;
+}): string {
+  if (
+    body.degrees[0] == 0 &&
+    body.degrees[1] == 0 &&
+    body.meters[0] == 0 &&
+    body.meters[1] == 0
+  ) {
+    return 'empty';
+  } else if (body.degrees[0] == 0 && body.degrees[1] == 0) {
+    return 'meters';
+  } else if (body.meters[0] == 0 && body.meters[1] == 0) {
+    return 'degrees';
+  } else {
+    return 'both meters and degrees can not have values';
   }
 }
--- src/coordinates/coordinates.service.ts
+++ src/coordinates/coordinates.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { Proj, Point, transform } from 'proj4';
+import { Proj, transform, toPoint } from 'proj4';
 @Injectable()
 export class CoordinatesService {
-  convertCoordinates(coordinate: { lat: number; lon: number }) {
-    const sourceProj = Proj('WGS84');
-    const destProj = Proj(
+  convertCoordinates(coordinate: { lat: number; lon: number }, unit: string) {
+    const degreeProj = Proj('WGS84');
+    const meterProj = Proj(
       '+proj=utm +zone=36 +south +datum=WGS84 +units=m +no_defs',
     );
     const x = coordinate.lon;
     const y = coordinate.lat;
-    const p = Point(x, y);
-    const r = transform(sourceProj, destProj, p);
+    const p = toPoint([x, y]);
 
-    return r;
+    if (unit == 'meters') {
+      const r = transform(meterProj, degreeProj, p);
+      return r;
+    } else {
+      const r = transform(degreeProj, meterProj, p);
+      return r;
+    }
   }
 }
