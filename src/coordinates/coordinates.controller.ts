48a2c6d9eba26e21f4ab35d3385d71c6785ea545
GISSIAH <62018768+GISSIAH@users.noreply.github.com>
2022-05-30 22:14:42 +0200

    formatting and enabled cors

--- src/coordinates/coordinates.controller.ts
+++ src/coordinates/coordinates.controller.ts
@@ -1,72 +1,74 @@
-import { Controller, Get, Post, Req, Res } from '@nestjs/common';
-import { Request, Response } from 'express';
-import { CoordinatesService } from './coordinates.service';
+import { Controller, Get, Post, Req, Res } from "@nestjs/common"
+import { Request, Response } from "express"
+import { CoordinatesService } from "./coordinates.service"
 
-@Controller('coordinates')
+@Controller("coordinates")
 export class CoordinatesController {
-  constructor(private readonly coordinateService: CoordinatesService) {}
-  @Post('convert')
-  convertCoordinates(@Req() req: Request, @Res() res: Response) {
-    /*
+    constructor(private readonly coordinateService: CoordinatesService) {}
+    @Post("convert")
+    convertCoordinates(@Req() req: Request, @Res() res: Response) {
+        /*
     const tempCoords = {
       lat: Number(req.body.lat),
       lon: Number(req.body.lon),
     };
 */
-    if (checkUnitToConvert(req.body) === 'empty') {
-      res.statusCode = 400;
-      res.send({ error: ' Found no coordinates in body' });
-    } else if (checkUnitToConvert(req.body) === 'meters') {
-      const tempCoords = {
-        lat: Number(req.body.meters[1]),
-        lon: Number(req.body.meters[0]),
-      };
-      const body = this.coordinateService.convertCoordinates(
-        tempCoords,
-        'meters',
-      );
-      res.send({
-        degrees: [body.x, body.y],
-        meters: req.body.meters,
-      });
-    } else if (checkUnitToConvert(req.body) === 'degrees') {
-      const tempCoords = {
-        lat: Number(req.body.degrees[1]),
-        lon: Number(req.body.degrees[0]),
-      };
+        if (checkUnitToConvert(req.body) === "empty") {
+            res.statusCode = 400
+            res.send({ error: " Found no coordinates in body" })
+        } else if (checkUnitToConvert(req.body) === "meters") {
+            const tempCoords = {
+                lat: Number(req.body.meters[1]),
+                lon: Number(req.body.meters[0]),
+            }
+            const body = this.coordinateService.convertCoordinates(
+                tempCoords,
+                "meters"
+            )
+            res.send({
+                degrees: [body.x, body.y],
+                meters: req.body.meters,
+            })
+        } else if (checkUnitToConvert(req.body) === "degrees") {
+            const tempCoords = {
+                lat: Number(req.body.degrees[1]),
+                lon: Number(req.body.degrees[0]),
+            }
 
-      const body = this.coordinateService.convertCoordinates(
-        tempCoords,
-        'degrees',
-      );
+            const body = this.coordinateService.convertCoordinates(
+                tempCoords,
+                "degrees"
+            )
 
-      res.send({
-        degrees: req.body.degrees,
-        meters: [body.x, body.y],
-      });
-    } else {
-      res.statusCode = 400;
-      res.send({ error: 'One pair must have the values you want to change' });
+            res.send({
+                degrees: req.body.degrees,
+                meters: [body.x, body.y],
+            })
+        } else {
+            res.statusCode = 400
+            res.send({
+                error: "One pair must have the values you want to change",
+            })
+        }
     }
-  }
 }
 
 function checkUnitToConvert(body: {
-  degrees: Array<number>;
-  meters: Array<number>;
+    degrees: Array<number>
+    meters: Array<number>
 }): string {
-  if (
-    body.degrees[0] == 0 &&
-    body.degrees[1] == 0 &&
-    body.meters[0] == 0 &&
-    body.meters[1] == 0
-  ) {
-    return 'empty';
-  } else if (body.degrees[0] == 0 && body.degrees[1] == 0) {
-    return 'meters';
-  } else if (body.meters[0] == 0 && body.meters[1] == 0) {
-    return 'degrees';
-  } else {
-    return 'both meters and degrees can not have values';
-  }
+    if (
+        body.degrees[0] == 0 &&
+        body.degrees[1] == 0 &&
+        body.meters[0] == 0 &&
+        body.meters[1] == 0
+    ) {
+        return "empty"
+    } else if (body.degrees[0] == 0 && body.degrees[1] == 0) {
+        return "meters"
+    } else if (body.meters[0] == 0 && body.meters[1] == 0) {
+        return "degrees"
+    } else {
+        return "both meters and degrees can not have values"
+    }
 }
