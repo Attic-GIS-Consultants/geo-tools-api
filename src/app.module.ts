05fb91c95a2eb46f0e8876787ab309c88da4b200
GISSIAH <62018768+GISSIAH@users.noreply.github.com>
2022-07-08 12:25:57 +0200

    Fix: changed env var name

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,11 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { CoordinatesService } from "./coordinates/coordinates.service";
 import { CoordinatesController } from "./coordinates/coordinates.controller";
-import { MapController } from "./map/map.controller";
 import { MongooseModule } from "@nestjs/mongoose";
 import { MapsModule } from "./map/map.module";
-//import { MapService } from "./map/map.service";
 import { ConfigModule } from "@nestjs/config";
 @Module({
-  imports: [ConfigModule.forRoot(), MapsModule, MongooseModule.forRoot(process.env.local_db)],
+  imports: [ConfigModule.forRoot(), MapsModule, MongooseModule.forRoot(process.env.db)],
   controllers: [AppController, CoordinatesController],
   providers: [AppService, CoordinatesService],
 })
